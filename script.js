af5da52c19b47e38a2df9d26d982d386ba211345
sorake14 <sorake14@gmail.com>
2023-07-25 20:24:58 -0300

    adicionando API

---
+++ script.js
@@ -0,0 +1,37 @@
+{const fundo = document.querySelector('body');
+        fundo.style.backgroundImage = 'url("./universe.jpg")';
+
+}
+fetch(' https://rickandmortyapi.com/api/character', {
+    method: 'GET'
+})
+.then(response => response.json())
+.then(function(json){
+
+    var container = document.querySelector('.container');
+
+    json.results.map(function(results){
+
+        container.innerHTML+=`
+            <div class= "card" data-id= ` + results.id + `>
+            <img src= ` + results.image +`> 
+            <strong> ` + results.name + ` </strong><br>
+            <span> ` + results.species +` </span><br>
+            </div>
+            `;
+
+    })
+    
+    // Adiciona o evento de clique para os cards
+    var cards = document.querySelectorAll('.card');
+    cards.forEach(function(card) {
+      card.addEventListener('click', function() {
+        var id = card.getAttribute('data-id');
+        var url = 'inf.html?id=' + id;
+        // Redireciona para a nova página com o ID específico
+         window.location.href = url
+      });
+      
+    });
+
+})
---
+++ script2.js
@@ -0,0 +1,27 @@
+var urlParams = new URLSearchParams(window.location.search);
+var id = urlParams.get('id');
+
+fetch('https://rickandmortyapi.com/api/character/' + id, {
+  method: 'GET'
+})
+  .then(response => response.json())
+  .then(function(data) {
+    var characterDetails = document.querySelector('.character-details');
+
+    characterDetails.innerHTML = `
+    <div class= "card-img">
+    <img src=` + data.image + ` >
+    </div>
+    <div class= "inf"> 
+      <h1> `+ data.name + ` </h1>
+      <p>Especie: ` + data.species + `</p>
+      <p>Cidade: ` + data.location.name + `</p>
+      <p>Genero: ` + data.gender + `</p>
+      <p>Status: ` + data.status + ` </p>
+      </div>
+    `;
+  });
+  
+ function redirectToPage() {
+    window.location.href = "/index.html";
+}
